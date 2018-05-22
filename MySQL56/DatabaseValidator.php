c606a8ffaf800af3c4e2da3f59a67b340296cf0c
Angel Sierra Vega <angel.sierra.vega@grupoindie.com>
2018-05-22 13:24:53 -0500

    18-05-20 - Functional show statement (0A.20) - Added functional component Platform (0A.26) 18-05-21 to 18-05-?? - New component Test (0A.2A) - New module ModuleDataModel (0A.2A) - New subpackage Validator (0A.2B)

---
+++ MySQL56/DatabaseValidator.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * GI-DBHandler-DVLP - DatabaseValidator
+ *
+ * @author Angel Sierra Vega <angel.sierra@grupoindie.com>
+ * @copyright (c) 2018 Angel Sierra Vega. Grupo INDIE.
+ *
+ * @package GIndie\DBHandler
+ *
+ * @version UNDEFINED
+ * @since 18-05-22
+ */
+
+namespace GIndie\DBHandler\MySQL56\Handler;
+
+/**
+ * Description of DatabaseValidator
+ *
+ * @author Angel Sierra Vega <angel.sierra@grupoindie.com>
+ */
+class DatabaseValidator
+{
+    //put your code here
+}
---
+++ MySQL56/ValidatorDatabase.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * GI-DBHandler-DVLP - ValidatorDatabase
+ *
+ * @author Angel Sierra Vega <angel.sierra@grupoindie.com>
+ * @copyright (c) 2018 Angel Sierra Vega. Grupo INDIE.
+ *
+ * @package GIndie\DBHandler
+ *
+ * @version 0A.2A
+ * @version DOING
+ * @since 18-05-22
+ */
+
+namespace GIndie\DBHandler\MySQL56\Handler;
+
+/**
+ * Description of ValidatorDatabase
+ *
+ * @author Angel Sierra Vega <angel.sierra@grupoindie.com>
+ */
+class ValidatorDatabase
+{
+    //put your code here
+}
